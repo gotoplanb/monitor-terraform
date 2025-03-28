f6ff353606ed7d5e198fbd53722c179bb3214e60
Dave Stanton <davestanton.us@gmail.com>
2025-03-28 13:41:07 -0400

    WIP fightings against Lambda redirect loop.

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,22 @@
-output "project_id" {
-  description = "The ID of the created Vercel project"
-  value       = vercel_project.monitor_client.id
-}
+# output "project_id" {
+#   description = "The ID of the created Vercel project"
+#   value       = vercel_project.monitor_client.id
+# }
 
-output "project_url" {
-  description = "The URL of the deployed project"
-  value       = "https://${vercel_project_domain.domain.domain}"
-}
+# output "project_url" {
+#   description = "The URL of the deployed project"
+#   value       = "https://${vercel_project_domain.domain.domain}"
+# }
 
-output "cloudflare_url" {
-  description = "The Cloudflare protected URL of the application"
-  value       = "https://${cloudflare_record.app.hostname}"
-}
+# output "cloudflare_url" {
+#  description = "The Cloudflare protected URL of the application"
+#  value       = "https://${cloudflare_record.app.hostname}"
+# }
 
-output "zero_trust_app_id" {
-  description = "The ID of the Zero Trust application"
-  value       = cloudflare_zero_trust_access_application.app.id
-}
+# output "zero_trust_app_id" {
+#  description = "The ID of the Zero Trust application"
+#  value       = cloudflare_zero_trust_access_application.app.id
+# }
 
 output "api_gateway_url" {
   description = "API Gateway URL"
